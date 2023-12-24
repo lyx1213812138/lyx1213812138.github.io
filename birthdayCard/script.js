0cf0920026733d9fe8861ce50ceefe38739ead56
lyx12138 <2742719109@qq.com>
2023-12-24 22:40:19 +0800

    这个贺卡已经用过了，先暂存起来

--- birthdayCard/script.js
+++ birthdayCard/script.js
@@ -37,29 +37,33 @@ initial();
 
 let trueDate1 = [11, 3], trueDate2 = [11, 8];
 function checkdate() {
-  let date = new Date();
-  console.log(date.getMonth(), date.getDate());
-  if (date.getMonth() <= trueDate1[0] - 1 && date.getDate() <= trueDate1[1]) {
-    if (date.getMonth() === trueDate1[0] - 1 && date.getDate() === trueDate1[1]) {
-      document.getElementById("cover").style.display = "none";
-      setTimeout(firework(), 10);
-    }
-    console.log("jyh");
-    document.title = "姜於含的生日";
-  } else if (date.getMonth() <= trueDate2[0] - 1 && date.getDate() <= trueDate2[1]) {
-    if (date.getMonth() === trueDate2[0] - 1 && date.getDate() === trueDate2[1]) {
+  // let date = new Date();
+  // console.log(date.getMonth(), date.getDate());
+  // if (date.getMonth() <= trueDate1[0] - 1 && date.getDate() <= trueDate1[1]) {
+  //   if (date.getMonth() === trueDate1[0] - 1 && date.getDate() === trueDate1[1]) {
+  //     document.getElementById("cover").style.display = "none";
+  //     setTimeout(firework(), 10);
+  //   }
+  //   console.log("jyh");
+  //   document.title = "姜於含的生日";
+  // } else if (date.getMonth() <= trueDate2[0] - 1 && date.getDate() <= trueDate2[1]) {
+  //   if (date.getMonth() === trueDate2[0] - 1 && date.getDate() === trueDate2[1]) {
+  //     document.getElementById("cover").style.display = "none";
+  //     document.getElementById("face").src = "birthdayCard/face2.jpg";
+  //     document.getElementById("text").src = "birthdayCard/text2.gif";
+  //     setTimeout(firework(), 10);
+  //   }
+  //   document.title = "高艺洋的生日";
+  //   // const inputcover = document.getElementById("inputcover");
+  //   // console.log(inputcover);
+  //   // if (inputcover.value === "lyx12138") {
+  //   //   cover.style.display = "flex";
+  //   // }
+  // }
       document.getElementById("cover").style.display = "none";
       document.getElementById("face").src = "birthdayCard/face2.jpg";
       document.getElementById("text").src = "birthdayCard/text2.gif";
       setTimeout(firework(), 10);
-    }
-    document.title = "高艺洋的生日";
-    // const inputcover = document.getElementById("inputcover");
-    // console.log(inputcover);
-    // if (inputcover.value === "lyx12138") {
-    //   cover.style.display = "flex";
-    // }
-  }
 }
 checkdate();
 
