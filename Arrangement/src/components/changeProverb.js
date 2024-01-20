ddfa867e91056ea0f87702fd8d81dcd8beab4dad
lyx12138 <2742719109@qq.com>
2024-01-20 14:39:42 +0800

    Update a little

--- Arrangement/src/components/changeProverb.js
+++ Arrangement/src/components/changeProverb.js
@@ -1,17 +1,17 @@
 import { watch } from 'vue';
 export default async function f(nowProverb, footerStyle) {
-  fetch('/src/assets/proverb.txt')
+  fetch('/src/assts/proverb.txt')
     .then(response => {
-      if (!response.ok) return 
+      if (!response.ok) return new Promise((_, rej) => rej(response.status));
       return response.text();
     })
     .then(fileContent => {
-      if(fileContent.indexOf('<') != -1)
-        return new Promise((_, rej) => rej(fileContent));
+      // if(fileContent.indexOf('<') != -1)
+      //   return new Promise((_, rej) => rej(fileContent));
+      console.log(fileContent);
       const proverbs = fileContent.split(/\r?\n/).filter(val => val);
       const randomNum = Math.floor(Math.random() * proverbs.length);
       nowProverb.value = proverbs[randomNum];
-      console.log(fileContent);
     })
     .catch(error => {
       console.error('cannot fetch proverbs:', error);
