357759392f835223cb4d4a3d9c17e8d337d058fb
lyx12138 <2742719109@qq.com>
2024-01-20 17:37:49 +0800

    aaa

--- Arrangement/src/components/saveAllTime.js
+++
@@ -1,8 +0,0 @@
-import { watch, ref } from 'vue';
-export default function saveAllTime(keyStr) {
-  const value = ref(localStorage.getItem(keyStr));
-  watch(value, (newValue) => {
-    localStorage.setItem(keyStr, newValue);
-  });
-  return value;
-};
